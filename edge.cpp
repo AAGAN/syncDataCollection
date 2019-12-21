b77adff4bf52ca541df47ddf50fe38185c131402
Arash Agan <AAGAN@users.noreply.github.com>
2019-12-20 19:07:23 -0500

    code used at the Turlock facility

--- edge.cpp
+++ edge.cpp
@@ -21,13 +21,16 @@ uint32_t p2 = 0;
 int numReadings = 0;
 int previousHour = 0;
 bool debug = false;
+uint32_t offset = 0;
+unsigned long previousWriteTime = 0;
+long Millis = 0;
 
 float convertToPressure(uint32_t rawVal)
 {
   float pressure;
   pressure = (float)rawVal - 406.0;
   pressure *= 0.092336;
-  if (pressure < -5)
+  if (pressure < -10)
     pressure = 0.0;
   return pressure;
 }
@@ -66,18 +69,31 @@ time_t getTeensy3Time()
 
 void writeData()
 {
-  time_t t = Teensy3Clock.get();
   // make a string for assembling the data to log:
+  //long val = long(millis() % 1000) - long(offset);
   String dataString = "";
-  dataString += String(t);
+  dataString += String(previousWriteTime);
   dataString += ".";
-  dataString += String(millis()%1000);
+  if(Millis<100){
+    dataString += "0";
+  }
+  dataString += String(Millis);//val >= 0 ? val : val + 1000);
   dataString += " , ";
   dataString += String(p0/numReadings);
   dataString += " , ";
   dataString += String(p1/numReadings);
   dataString += " , ";
   dataString += String(p2/numReadings);
+  Millis += 50;
+  if (debug){
+    // Serial.print(offset);
+    // Serial.print(" - ");
+    // Serial.print(val);
+    // Serial.print(" - ");
+    // Serial.println(1000+val);
+  }
+  // dataString += " , ";
+  // dataString += String(millis()%1000);
 
   File dataFile = SD.open(filename, FILE_WRITE);
   // if the file is available, write to it:
@@ -109,49 +125,6 @@ int sendData(u_int32_t t)
   return 0;
 }
 
-//sends the set time from Teensy and reads an average pressure readings and sends it for confirmation
-void sendSetTimeAndPressure()
-{
-  time_t t = Teensy3Clock.get();
-  sendData(t);
-  p0 = 0;
-  p1 = 0;
-  p2 = 0;
-  for (int i = 0; i<20;i++)
-  {
-    p0 += analogRead(A0);
-    delay(2);// 1 may be better?
-    p1 += analogRead(A1);
-    delay(2);
-    p2 += analogRead(A2);
-    delay(2);
-  }
-  p0 /= 20;
-  p1 /= 20;
-  p2 /= 20;
-  delay(250);//150 may be better?
-  sendData(p0);
-  delay(250);
-  sendData(p1);
-  delay(250);  
-  sendData(p2);
-  delay(250);
-  if (sdSuccessSwitch)
-  {
-    sendData(1);
-  }
-  else
-  {
-    sendData(0);
-  }
-  
-  p0 = 0;
-  p1 = 0;
-  p2 = 0;
- //delay(150);
- //flushAPI();//to clear the acknowledgements?
-}
-
 void flushAPI()
 {
   //XBeeResponse discard;
@@ -164,6 +137,58 @@ void flushAPI()
     xbee.readPacket();
     //xbee.getResponse(discard);
   }
+
+  // This must be correct too
+  // while(xbee.readPacket()){
+  //   xbee.readPacket();
+  // }
+}
+
+//sends the set time from Teensy and reads an average pressure readings and sends it for confirmation
+void sendSetTimeAndPressure()
+{
+  time_t t = Teensy3Clock.get();
+  sendData(t);
+  p0 = 0;
+  p1 = 0;
+  p2 = 0;
+  for (int i = 0; i<20;i++)
+  {
+    p0 += analogRead(A0);
+    delay(1);
+    p1 += analogRead(A1);
+    delay(1);
+    p2 += analogRead(A2);
+    delay(1);
+  }
+  p0 /= 20;
+  p1 /= 20;
+  p2 /= 20;
+  delay(100);
+  sendData(p0);
+  delay(100);
+  flushAPI();
+  sendData(p1);
+  delay(100);
+  flushAPI();
+  sendData(p2);
+  delay(100);
+  flushAPI();
+  if (sdSuccessSwitch)
+  {
+    sendData(1);
+  }
+  else
+  {
+    sendData(0);
+  }
+  
+  p0 = 0;
+  p1 = 0;
+  p2 = 0;
+
+  delay(100);
+  flushAPI();
 }
 
 void setup()
@@ -175,15 +200,16 @@ void setup()
   setSyncInterval(10);
   if(debug){
     Serial.begin(115200);
+    delay(50);
   }
   Serial1.begin(115200);
-  //delay(5);
+  delay(50);
   analogReadResolution(12);
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   xbee.setSerial(Serial1);
-  delay(15000);
+  delay(5000);
   if(debug){
     Serial.print("Initializing SD card...");
   }
@@ -219,7 +245,7 @@ void loop()
       uint8_t frameData[] = {resp.getData(0),resp.getData(1),resp.getData(2),resp.getData(3)};
       uint32_t receivedTime = decodePayload(frameData);
       if(debug){
-        Serial.print(receivedTime);
+        Serial.println(receivedTime);
       }
       if(receivedTime>1000000)//then this must be the unix time
       {
@@ -240,27 +266,33 @@ void loop()
       else if (receivedTime == 0)
       {
         writeSwitch = false;
-        //flushAPI();
+        flushAPI();
       }
     } 
   }
-  if (millis() - recordingMillis > 100 && writeSwitch)
+  bool nextSecond = Teensy3Clock.get() != previousWriteTime;
+  if ((millis() - recordingMillis >= 50) && writeSwitch && sdSuccessSwitch)
   {
-    writeData();
     recordingMillis = millis();
+    if(nextSecond){
+      //offset = millis() % 1000;
+      previousWriteTime = Teensy3Clock.get();
+      Millis = 0;
+    }
+    writeData();
     //Serial.println(numReadings);
     numReadings = 0;//initialize after writing to file
     p0 = 0;//initialize after writing to file
     p1 = 0;//initialize after writing to file
     p2 = 0;//initialize after writing to file
   }
-  else if (writeSwitch && millis()-averagingMillis>5)//averaging the readings
+  if (writeSwitch && millis()-averagingMillis >= 2)//averaging the readings
   {
+    averagingMillis = millis();
     p0 += analogRead(A0);
     p1 += analogRead(A1);
     p2 += analogRead(A2);
     numReadings++;
-    averagingMillis = millis();
   }
   if (sendPressureSwitch)//everytime time is received, send the set time and pressures
   {
